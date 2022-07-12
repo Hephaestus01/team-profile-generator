404d6f6fefc935a109bd97bb50cb4da77a2d0afc
Matt <mwhlavaty@gmail.com>
2022-07-12 00:26:41 -0700

    work on generating page, prompts, Employee class

--- app.js
+++ app.js
@@ -1,3 +1,67 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
 const generatePage = require('./src/page-template');
+const Employee = require('./lib/Employee');
 
+// add questions
+const promptUser = () => {
+
+    return inquirer
+        .prompt([
+            {
+            type: 'input',
+            name: 'name',
+            message: 'Enter team manager name:'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter team manager Employee ID (Default: 1):',
+                default: '1'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter team manager e-mail address:'
+            },
+            {
+                type: 'number',
+                name: 'office',
+                message: 'Enter team manager office number:'
+            }
+        ])
+        
+        
+        .then(({ name, id, email, office }) => {
+            this.employee = new Employee(name);
+        })
+
+
+}
+
+
+// function to initialize app
+function init() {
+    promptUser()
+        .then(employeeData => {
+            return generatePage(employeeData);
+        })
+        .then(pageHtml => {
+            return new Promise((resolve, reject) => {
+                fs.writeFile('./dist/index.html', pageHtml, err => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    resolve({
+                        ok: true,
+                        message: 'File created'
+                    })
+                })
+            })
+        })
+};
+
+// initialize app
+init();
