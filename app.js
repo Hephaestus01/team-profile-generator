6a0571b9fc1c021eca7a259efe05602b3a96c0b1
Matt <mwhlavaty@gmail.com>
2022-07-12 01:16:03 -0700

    Complete basic prompts

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const promptUser = () => {
     return inquirer
         .prompt([
             {
-            type: 'input',
-            name: 'name',
-            message: 'Enter team manager name:'
+                type: 'input',
+                name: 'name',
+                message: 'Enter team manager name:'
             },
             {
                 type: 'input',
@@ -28,21 +28,65 @@ const promptUser = () => {
                 type: 'number',
                 name: 'office',
                 message: 'Enter team manager office number:'
-            }
+            },
+
         ])
-        
-        
+
         .then(({ name, id, email, office }) => {
             this.employee = new Employee(name);
+            this.employee = new Employee(id);
+            this.employee = new Employee(email);
+            this.employee = new Employee(office);
         })
+};
 
+const promptRoster = () => {
 
-}
+    return inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'addEmployee',
+                message: 'Would you like to add an employee to the roster?',
+                default: false
+            },
+            {
+                type: 'list',
+                name: 'employeeRole',
+                message: 'Choose employee role:',
+                choices: ['Engineer', 'Intern']
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter Employee ID number:',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "Enter employee's email address:",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "Enter employee's GitHub username:",
+                when: ({ employeeRole }) => employeeRole === 'Engineer'
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "Enter intern's school:",
+                when: ({ employeeRole }) => employeeRole === 'Intern'
+            }
+        ])
+};
+
 
 
 // function to initialize app
 function init() {
     promptUser()
+        .then(promptRoster)
         .then(employeeData => {
             return generatePage(employeeData);
         })
